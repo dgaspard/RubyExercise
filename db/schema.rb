0140315407c5dced04e81eb8bf86f1faba262ea8
Nicola Paolucci <npaolucci@atlassian.com>
2022-08-21 13:11:36 -0500

    manual article db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_21_165218) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_21_175529) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "marticles", force: :cascade do |t|
+    t.string "mtitle"
+    t.text "mdescription"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
