ea51535486af25d9d914d803237ba9207150ef63
Nicola Paolucci <npaolucci@atlassian.com>
2022-08-21 19:37:23 -0500

    Error validation on manual add

--- app/controllers/marticles_controller.rb
+++ app/controllers/marticles_controller.rb
@@ -3,4 +3,22 @@ class MarticlesController < ApplicationController
         def show
           @marticle = Marticle.find(params[:id])       
         end
+
+        def index
+          @marticles = Marticle.all
+        end
+
+        def new
+          @marticle = Marticle.new
+        end
+
+        def create
+           @marticle = Marticle.new(params.require(:marticle).permit(:mtitle, :mdescription))
+           if @marticle.save
+                flash[:notice] = "Marticle was created successfully"
+                redirect_to @marticle        
+           else
+                render 'new', status: :unprocessable_entity  
+           end
+        end
 end
