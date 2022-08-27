e667e670c6ea6a8317f294e082710ddaab876813
Nicola Paolucci <npaolucci@atlassian.com>
2022-08-27 06:55:13 -0500

    Basic Crud operations created manually

--- app/controllers/marticles_controller.rb
+++ app/controllers/marticles_controller.rb
@@ -1,7 +1,8 @@
 class MarticlesController < ApplicationController
 
+        before_action :set_marticle, only: [:show, :edit, :update, :destroy]
+
         def show
-          @marticle = Marticle.find(params[:id])       
         end
 
         def index
@@ -12,13 +13,41 @@ class MarticlesController < ApplicationController
           @marticle = Marticle.new
         end
 
+        
+        def edit
+        end
+
         def create
-           @marticle = Marticle.new(params.require(:marticle).permit(:mtitle, :mdescription))
+           @marticle = Marticle.new(marticle_params)
            if @marticle.save
                 flash[:notice] = "Marticle was created successfully"
                 redirect_to @marticle        
            else
                 render 'new', status: :unprocessable_entity  
            end
+        end
+
+        def update
+          if @marticle.update(marticle_params)
+            flash[:notice] = "Marticle was updated successfully"
+            redirect_to @marticle
+          else
+            render 'edit', status: :unprocessable_entity 
+          end
+        end
+
+        def destroy
+          @marticle.destroy
+          redirect_to marticles_path
+        end
+
+        private
+
+        def set_marticle   
+          @marticle = Marticle.find(params[:id])
+        end
+
+        def marticle_params 
+          params.require(:marticle).permit(:mtitle, :mdescription)
         end
 end
